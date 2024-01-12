f5c7dd187945f26686bb84a5ad607914052be366
BennyMota <bennyniice@icloud.com>
2024-01-12 18:56:52 +0200

    Add a few more buttons and a scrollbar

--- Task_1_To_Do_List/To_do_list.py
+++ Task_1_To_Do_List/To_do_list.py
@@ -12,11 +12,30 @@ def add_task():
         entry_task.delete(0, tkinter.END)
     else:
         tkinter.messagebox.showwarning(title="Warning!", message="You must add a task")
+        
+def delete_task():
+    pass
+
+def save_task():
+    pass
+
+def load_task():
+    pass
 
 
 #Creating the GUI
-listbox_tasks = tkinter.Listbox(root, height = 10, width = 50)
-listbox_tasks.pack()
+
+frame_tasks = tkinter.Frame(root)
+frame_tasks.pack()
+
+listbox_tasks = tkinter.Listbox(frame_tasks, height = 10, width = 50)
+listbox_tasks.pack(side=tkinter.LEFT)
+
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+listbox_tasks.config(yscrollcommand=scrollbar_tasks)
+scrollbar_tasks.config(command=listbox_tasks.yview)
 
 entry_task = tkinter.Entry(root, width=50)
 entry_task.pack()
@@ -24,13 +43,13 @@ entry_task.pack()
 button_add_task = tkinter.Button(root, text="Add Task", width = 48, command=add_task)
 button_add_task.pack()
 
+button_delete_task = tkinter.Button(root, text="Delete Task", width = 48, command=delete_task)
+button_delete_task.pack()
 
+button_save_task = tkinter.Button(root, text="Save Task", width = 48, command=save_task)
+button_save_task.pack()
 
-
-
-
-
-
-
+button_load_task = tkinter.Button(root, text="Load Task", width = 48, command=load_task)
+button_load_task.pack()
 
 root.mainloop()
